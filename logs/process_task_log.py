d70d68aee287417cf5bda148cfd941825dafb5e9
duihuhu <duihuhu@163.com>
2022-10-27 16:06:22 +0800

    debug add push normal task

--- logs/process_task_log.py
+++ logs/process_task_log.py
@@ -44,6 +44,8 @@ if __name__ == "__main__":
     exec_task_sign = "hucc time for exec task time"
     exec_task_callback_sign = "hucc time for exec task callback to lanaguage time"
     exec_task_args_sign = "hucc time for exec task args"
+
+    push_task_sign = "hucc push normal task"
     sign = [get_object_mem_sign, get_object_plasma_sign, exec_task_sign, exec_task_callback_sign, exec_task_args_sign]
     datapath = "/tmp/ray/session_latest/logs"
 
@@ -74,3 +76,27 @@ if __name__ == "__main__":
     mul_sum_time = calculate_time(merge_time)
     print("mul_time", ": ", mul_sum_time)
 
+    push_task_start = {}
+    push_task_end = {}
+    for file in os.listdir(datapath):
+        if core_worker_file_sign in file:
+            with open(datapath + "/" + file, "r") as fd:
+                for line in fd.readlines():
+                    if push_task_sign in line:
+                        if 'start' in line:
+                            sign_sentence = line.split("\n")[0].split(" ")
+                            push_task_start[sign_sentence[-8]] = float(sign[-1])
+                        if 'end' in line:
+                            sign_sentence = line.split("\n")[0].split(" ")
+                            push_task_end[sign_sentence[-8]] = float(sign[-1])
+   muti_time = []
+    for k,v in push_task_start:
+        push_task_interval = []
+        if push_task_end.get(k):
+            push_task_interval.append(v)
+            push_task_interval.append(push_task_end.get(k))
+        if push_task_interval:
+            muti_time.append(push_task_interval)
+    merge_time = merge(muti_time)
+    mul_sum_time = calculate_time(muti_time)
+    print("push task time:", mul_sum_time)
