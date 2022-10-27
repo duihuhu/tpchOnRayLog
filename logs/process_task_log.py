bcae97cf51732fe9662560788525eebfb341b16c
duihuhu <duihuhu@163.com>
2022-10-27 11:47:22 +0800

    add mul_time

--- logs/process_task_log.py
+++ logs/process_task_log.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     get_object_plasma_sign = "hucc time for get object from plasma"
     exec_task_sign = "hucc time for exec task time"
     exec_task_callback_sign = "hucc time for exec task callback to lanaguage time"
-    exec_task_args_sign = "hucc time for exec task args "
+    exec_task_args_sign = "hucc time for exec task args"
     sign = [get_object_mem_sign, get_object_plasma_sign, exec_task_sign, exec_task_callback_sign, exec_task_args_sign]
     datapath = "/tmp/ray/session_latest/logs"
 
